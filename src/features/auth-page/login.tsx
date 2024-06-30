b33b653a4105f94eaa98a034a2fff888d5e99480
JayanthiArthi <37687452+JayanthiArthi@users.noreply.github.com>
2024-06-30 16:04:44 +0530

    Update login.tsx

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -18,24 +18,22 @@ interface LoginProps {
 
 export const LogIn: FC<LoginProps> = (props) => {
   return (
-    <Card className="flex gap-2 flex-col min-w-[300px]">
+    <Card className="flex gap-2 flex-col min-w-[500px] shadow-xl shadow-cyan-500/50 ">
       <CardHeader className="gap-2">
         <CardTitle className="text-2xl flex gap-2">
-          <Avatar className="h-8 w-8">
+          <Avatar className="h-20 w-20">
             <AvatarImage src={"ai-icon.png"} />
           </Avatar>
-          <span className="text-primary">{AI_NAME}</span>
+          <span className="text-primary text-3xl flex items-center">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your GitHub or Microsoft 365 account
+          <span className="text-2xl">
+          Login in with your Microsoft 365 account
+          </span>
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
         <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
-        <Button onClick={() => signIn("localdev")}>
-            Basic Auth (DEV ONLY)
-        </Button>
       </CardContent>
     </Card>
   );
